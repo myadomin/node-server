929a8cb196a7287bb277b079d98e8b5de455b775
luominmin <luominmin@qtrade.com.cn>
2018-12-29 18:12:32 +0800

    add gitignore

--- db/index.js
+++ db/index.js
@@ -1,10 +1,11 @@
 // 修改root密码  ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 const mysql = require('mysql');
+const config = require('./config');
 const pool = mysql.createPool({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'password',
-  database : 'world'
+  host     : config.host,
+  user     : config.user,
+  password : config.password,
+  database : config.database
 });
 
 const query = (sql, callback) => {
